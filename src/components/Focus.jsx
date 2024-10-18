c567dc4f65f2f335d306883529be4e72ac2c2030
DvUo <dvuo@proton.me>
2024-10-18 08:58:28 -0300

    add finishs css

--- src/components/Focus.jsx
+++ src/components/Focus.jsx
@@ -1,23 +1,20 @@
-
 import { Offcanvas } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Focus.css";
 
-
-export function OffCanvasFocus({ children,onOpen,onClose}) {
-
-
-
+export function OffCanvasFocus({ children, onOpen, onClose }) {
   return (
     <>
-      <Offcanvas show={onOpen} onHide={onClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>AddTask</Offcanvas.Title>
+      <Offcanvas show={onOpen} onHide={onClose} className="offcanvas-custom">
+        <Offcanvas.Header closeButton className="offcanvas-header-custom">
+          <Offcanvas.Title className="offcanvas-title-custom">
+            Add Task
+          </Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body>{children}</Offcanvas.Body>
+        <Offcanvas.Body className="offcanvas-body-custom">
+          {children}
+        </Offcanvas.Body>
       </Offcanvas>
     </>
-
   );
 }
-
-
